9d12a13129219f12d4da1c31849b9ea44438e472
shweta_pccm <shwetakadu.prashanticancercare@gmail.com>
2021-06-30 17:04:43 +0530

    surgery_videos date extracted

--- sorting_surgery_images.py
+++ sorting_surgery_images.py
@@ -2,10 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 import datetime
+import re
+import itertools
 
 folder_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019'
-folder_path1 = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2019 IMAGES'
-folder_path2 = 'Z:\\Clinical_Database\\surgery_ot_data'
+# folder_path1 = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2019 IMAGES'
+# folder_path2 = 'Z:\\Clinical_Database\\surgery_ot_data'
 
 folder_names = os.listdir(folder_path)
 
@@ -15,60 +17,244 @@ def get_summary_dir_subdir(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             dir_path = os.path.dirname(file_path)
+            # dir_name = os.path.dirname(dir_path)
+            # print(dir_name)
             file_size = os.stat(file_path).st_size/float(1 << 20)
             file_size_rounded = round(file_size, 1)
             file_paths_name = np.append(dir_path, file)
             file_dat = np.append(file_paths_name, file_size_rounded)
             files_summary.append(file_dat)
             file_df = pd.DataFrame(files_summary, columns=['directory_path', 'file_name', 'file_size_in_MB'])
-            print(file, file_size)
+            print(file)
     return file_df
 
-df = get_summary_dir_subdir(folder_path)
-df.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2014-2019)_sk.xlsx', index=False)
+# df = get_summary_dir_subdir(folder_path)
+# df.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2014-2019)_sk.xlsx', index=False)
 
-df1 = get_summary_dir_subdir(folder_path1)
-df1.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2019)_sk.xlsx', index=False)
+df = pd.read_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2014-2019)_sk.xlsx')
 
-df2 = get_summary_dir_subdir(folder_path2)
-df2.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(ot_data_2021)_sk.xlsx', index=False)
+# df1 = get_summary_dir_subdir(folder_path1)
+# df1.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2019)_sk.xlsx', index=False)
+#
+# df2 = get_summary_dir_subdir(folder_path2)
+# df2.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(ot_data_2021)_sk.xlsx', index=False)
 
-def get_summary_dir_subdir(folder_path):
-    files_summary = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_size = os.stat(file_path).st_size/float(1 << 20)
-            file_size_rounded = round(file_size, 1)
-            file_info = np.append(file_path, file)
-            file_dat = np.append(file_info, file_size_rounded)
-            files_summary.append(file_dat)
-            file_df = pd.DataFrame(files_summary, columns=['path', 'file_name', 'file_size_in_MB'])
-            print(file, file_size)
-    return file_df
+dir_path = 'Y:\\RESEARCH\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\Sunita Mali\\6-28-2016'
 
-def get_file_name_size_time(file):
-    file_size = file.stat().st_size/float(1 << 20)
-    file_cr_time = datetime.datetime.fromtimestamp(file.stat().st_ctime)
-    file_mo_time = datetime.datetime.fromtimestamp(file.stat().st_mtime)
-    # print(file_size, file_cr_time, file_mo_time)
-    file_name_size = np.append(file.name, file_size)
-    file_cr_mo = np.append(file_cr_time, file_mo_time)
-    output_lst = np.append(file_name_size, file_cr_mo)
-    return output_lst
+patterns = ['\d{4}-\d{2}-\d{2}', '\d{4}-\d{1}-\d{1}', '\d{4}-\d{2}-\d{1}', '\d{4}-\d{1}-\d{2}',
+            '\d{2}-\d{2}-\d{4}', '\d{1}-\d{1}-\d{4}','\d{1}-\d{2}-\d{4}', '\d{2}-\d{1}-\d{4}',]
 
-def get_file_name_size_if_dir(directory):
-    file_data_from_dir = []
-    file_names = os.scandir(directory)
-    for file_name in file_names:
-        if file_name.is_file():
-            if os.path.exists(file_name):
-                output_lst = get_file_name_size_time(file_name)
-                file_data_from_dir.append(output_lst)
-            else:
-                output_lst = [file_name.name, 'not_exist', 'not_exist', 'not_exist']
-                file_data_from_dir.append(output_lst)
-    return file_data_from_dir
+patterns_dict = {'%Y-%m-%d': ['\d{4}-\d{2}-\d{2}', '\d{4}-\d{1}-\d{1}', '\d{4}-\d{2}-\d{1}', '\d{4}-\d{1}-\d{2}'],
+                 '%Y-%d-%m': ['\d{4}-\d{1}-\d{2}', '\d{4}-\d{1}-\d{1}', '\d{4}-\d{2}-\d{1}', '\d{4}-\d{1}-\d{2}'],
+                 '%m-%d-%Y': ['\d{1}-\d{1}-\d{4}', '\d{1}-\d{2}-\d{4}', '\d{2}-\d{2}-\d{4}', '\d{2}-\d{1}-\d{4}'],
+                 '%d-%m-%Y': ['\d{2}-\d{1}-\d{4}', '\d{2}-\d{2}-\d{4}', '\d{1}-\d{1}-\d{4}', '\d{1}-\d{2}-\d{4}']
+                 }
+
+def get_value_from_key(pattern_dict, value):
+    id_pos = [value in value_list for value_list in (pattern_dict.values())]
+    key_reqd = list(itertools.compress(pattern_dict.keys(), id_pos))
+    return key_reqd
+
+def get_matched_dt_pattern(patterns, patterns_dict, dir_path):
+    matched_dts = []
+    for pattern in patterns:
+        match = re.search(pattern, str(dir_path))
+        print(match)
+        if match is not None:
+            val = get_value_from_key(patterns_dict, pattern)
+            # print(val)
+            # print(len(val))
+            if len(val) == 1:
+                try:
+                    dt = datetime.datetime.strptime(match.group(), val[0]).date()
+                    matched_dts.append(dt)
+                    # print(dt)
+                except ValueError:
+                    dt = datetime.datetime.strptime(match.group(), val[0][3:]).date()
+                    matched_dts.append(dt)
+                    # print(dt)
+                except:
+                    print('not_matched')
+            elif len(val) == 2:
+                try:
+                    dt = datetime.datetime.strptime(match.group(), val[0]).date()
+                    matched_dts.append(dt)
+                    dt1 = datetime.datetime.strptime(match.group(), val[1]).date()
+                    matched_dts.append(dt1)
+                    # print(dt1)
+                except ValueError:
+                    dt = datetime.datetime.strptime(match.group(), val[0][3:]).date()
+                    matched_dts.append(dt)
+                    dt1 = datetime.datetime.strptime(match.group(), val[1][3:]).date()
+                    matched_dts.append(dt1)
+                    # print(dt1)
+                except:
+                    print('not_matched')
+            elif len(val) == 3:
+                try:
+                    dt = datetime.datetime.strptime(match.group(), val[0]).date()
+                    matched_dts.append(dt)
+                    dt1 = datetime.datetime.strptime(match.group(), val[1]).date()
+                    matched_dts.append(dt1)
+                    # print(dt1)
+                    dt2 = datetime.datetime.strptime(match.group(), val[2]).date()
+                    matched_dts.append(dt2)
+                    # print(dt2)
+                except ValueError:
+                    dt = datetime.datetime.strptime(match.group(), val[0][3:]).date()
+                    matched_dts.append(dt)
+                    dt1 = datetime.datetime.strptime(match.group(), val[1][3:]).date()
+                    matched_dts.append(dt1)
+                    # print(dt1)
+                    dt2 = datetime.datetime.strptime(match.group(), val[2][3:]).date()
+                    matched_dts.append(dt2)
+                    # print(dt2)
+                except:
+                    print('not_matched')
+    return matched_dts
+
+matched_dts = get_matched_dt_pattern(patterns, patterns_dict, dir_path)
+
+def get_sub_dirs_dates(patterns, patterns_dict, df, dir_name_str = 'directory_path'):
+    dts = []
+    for dir_path in df[dir_name_str]:
+        print(dir_path)
+        dt = get_matched_dt_pattern(patterns, patterns_dict, dir_path)
+        dts.append(dt)
+        print(dt)
+    return dts
+
+dts = get_sub_dirs_dates(patterns, patterns_dict, df, dir_name_str = 'directory_path')
+df['image_dates'] = dts
+df.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_24_extracted_dates_sk.xlsx')
+pd.to_datetime(df.image_dates, '%m-%d-%Y')
+##
+
+def get_matched_dt_pattern(patterns, patterns_dict, dir_path):
+    matched_dts = []
+    for pattern in patterns:
+        match = re.search(pattern, str(dir_path))
+        print(match)
+        if match is not None:
+            val = get_value_from_key(patterns_dict, pattern)
+            print(val)
+            print(len(val))
+            try:
+                dt = datetime.datetime.strptime(match.group(), val[0]).date()
+                matched_dts.append(dt)
+                print(dt)
+            except ValueError:
+                dt1 = datetime.datetime.strptime(match.group(), val[1]).date()
+                matched_dts.append(dt1)
+                print(dt1)
+            except:
+                matched_dts.append('NA')
+                print('error')
+    return matched_dts
+
+
+def get_matched_groups(patterns, dir_path):
+    matched_patterns = []
+    for pattern in patterns:
+        print(pattern)
+        match = re.search(pattern, str(dir_path))
+        print(match)
+        if match is not None:
+            lst = np.append(match, pattern)
+            matched_patterns.append(lst)
+    return matched_patterns
+
+dir_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\Zubeida Shaikh(RC)\\10-12-2015'
+matched_patterns = get_matched_groups(patterns, dir_path)
+
+for matched_pattern in matched_patterns:
+    val = get_value_from_key(patterns_dict, matched_pattern[0][0])
+    print(matched_pattern, val)
+
+
+# def get_matched_dt_pattern(patterns, patterns_dict, dir_path):
+#     matched_dts = []
+#     for pattern in patterns:
+#         match = re.search(pattern, str(dir_path))
+#         if match is not None:
+#             val = get_value_from_key(patterns_dict, pattern)
+#             # print(match, val)
+#             try:
+#                 dt = datetime.datetime.strptime(match.group(), val[0]).date()
+#                 matched_dts.append(dt)
+#             except ValueError:
+#                 # try:
+#                 #     '%d-%Y'
+#                 matched_dts.append('not_available')
+#             # print(dt)
+#     return matched_dts
+
+
+# def get_date_image(patterns, dir_path):
+#     matched_dict = get_matched_dt_pattern(patterns, dir_path)
+#     for matched_pattern in matched_dict.values():
+#         if matched_pattern is not None:
+#             val = get_value_from_key(matched_dict, matched_pattern)
+#             print(val)
+#             dt = datetime.datetime.strptime(val.group(), val[0]).date()
+#             print(dt)
+#             return dt
+#         else:
+#             return 'NA'
+#
+# get_date_image(patterns, dir_path)
+
+# def get_sub_dirs_dates(patterns, patterns_dict, df, dir_name_str = 'directory_path'):
+#     dts = []
+#     for dir_path in df[dir_name_str]:
+#         print(dir_path)
+#         dt = get_matched_dt_pattern(patterns, patterns_dict, dir_path)
+#         dts.append(dt)
+#         print(dt)
+#     return dts
+
+# dts = get_sub_dirs_dates(patterns, patterns_dict, df, dir_name_str = 'directory_path')
+
+# dir_path = 'Y:\RESEARCH\RESEARCH-TEAM\Smita Sister\All surgical images\2014-June2019\Vrushali Nalawade\8-7-2017'
+# df['image_dates'] = dts
+
+
+# def get_summary_dir_subdir(folder_path):
+#     files_summary = []
+#     for root, dirs, files in os.walk(folder_path):
+#         for file in files:
+#             file_path = os.path.join(root, file)
+#             file_size = os.stat(file_path).st_size/float(1 << 20)
+#             file_size_rounded = round(file_size, 1)
+#             file_info = np.append(file_path, file)
+#             file_dat = np.append(file_info, file_size_rounded)
+#             files_summary.append(file_dat)
+#             file_df = pd.DataFrame(files_summary, columns=['path', 'file_name', 'file_size_in_MB'])
+#             print(file, file_size)
+#     return file_df
+
+# def get_file_name_size_time(file):
+#     file_size = file.stat().st_size/float(1 << 20)
+#     file_cr_time = datetime.datetime.fromtimestamp(file.stat().st_ctime)
+#     file_mo_time = datetime.datetime.fromtimestamp(file.stat().st_mtime)
+#     # print(file_size, file_cr_time, file_mo_time)
+#     file_name_size = np.append(file.name, file_size)
+#     file_cr_mo = np.append(file_cr_time, file_mo_time)
+#     output_lst = np.append(file_name_size, file_cr_mo)
+#     return output_lst
+
+# def get_file_name_size_if_dir(directory):
+#     file_data_from_dir = []
+#     file_names = os.scandir(directory)
+#     for file_name in file_names:
+#         if file_name.is_file():
+#             if os.path.exists(file_name):
+#                 output_lst = get_file_name_size_time(file_name)
+#                 file_data_from_dir.append(output_lst)
+#             else:
+#                 output_lst = [file_name.name, 'not_exist', 'not_exist', 'not_exist']
+#                 file_data_from_dir.append(output_lst)
+#     return file_data_from_dir
 
 def get_output_df(folder_path):
     output_lst = []
---
+++ surgery_images_extracting_dates.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+import re
+import datetime
+
+df = pd.read_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2014-2019)_sk.xlsx')
+df1 = pd.read_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(2019)_sk.xlsx')
+df2 = pd.read_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_23_surgery_images_info(ot_data_2021)_sk.xlsx')
+
+# string = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\2014\\12-13-2014'
+# splitted_str = string.replace(folder_path, '')
+#
+# substring = '\\'
+# lst = splitted_str.replace(substring, ',')
+# lst1 = re.split(',', lst)
+
+def split_dir_path(df, dir_path_name_str = 'directory_path',
+                   parent_folder_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\',
+                   substring = '\\'):
+    spllited_dir = []
+    bracket_info_lst = []
+    for dir_path in df[dir_path_name_str]:
+        child_dir = dir_path.replace(parent_folder_path, '')
+        split_dir = child_dir.replace(substring, ',')
+        spllited_lst = re.split(',', split_dir)
+        print(spllited_lst)
+        lenght = len(spllited_lst)
+        print(lenght)
+        bracket_info_gr = re.search(r"\(([-A-Za-z0-9_ ]+)\)", dir_path)
+        if bracket_info_gr is not None:
+            bracket_info = bracket_info_gr.group(1)
+            print(bracket_info)
+            bracket_info_lst.append(bracket_info)
+        else:
+            bracket_info_lst.append('nothing_in_brackets')
+        len_lst = np.append(lenght, spllited_lst)
+        spllited_dir.append(len_lst)
+        output_df = pd.DataFrame(spllited_dir, columns=['length', 'dir_name', 'dir_name1', 'dir_name2', 'dir_name3', 'dir_name4', 'dir_name5'])
+        output_df.insert(0, 'info_from_brackets', bracket_info_lst)
+    return output_df
+
+
+output_df = split_dir_path(df, dir_path_name_str = 'directory_path',
+                   parent_folder_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\',
+                   substring = '\\')
+
+final_df = pd.concat([df, output_df], axis=1)
+
+final_df.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_25_sub_dirs(2014-2019)_sk.xlsx', index=False)
+
+##
+output_df1 = split_dir_path(df1, dir_path_name_str = 'directory_path',
+                   parent_folder_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2019 IMAGES\\',
+                   substring = '\\')
+
+final_df1 = pd.concat([df1, output_df1], axis=1)
+
+final_df1.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_25_sub_dirs(2019)_sk.xlsx', index=False)
+##
+def split_dir_path_for_2021(df, dir_path_name_str = 'directory_path',
+                   parent_folder_path = 'Y:\\RESEARCH\\RESEARCH-TEAM\\Smita Sister\\All surgical images\\2014-June2019\\',
+                   substring = '\\', date_digit_pattern = '\d{8}'):
+    spllited_dir = []
+    bracket_info_lst = []
+    video_dates = []
+    for dir_path in df[dir_path_name_str]:
+        child_dir = dir_path.replace(parent_folder_path, '')
+        split_dir = child_dir.replace(substring, ',')
+        spllited_lst = re.split(',', split_dir)
+        print(spllited_lst)
+        lenght = len(spllited_lst)
+        print(lenght)
+        bracket_info_gr = re.search(r"\(([-A-Za-z0-9_ ]+)\)", dir_path)
+        if bracket_info_gr is not None:
+            bracket_info = bracket_info_gr.group(1)
+            print(bracket_info)
+            bracket_info_lst.append(bracket_info)
+        else:
+            bracket_info_lst.append('nothing_in_brackets')
+        int_gr = re.search(date_digit_pattern, dir_path)
+        if int_gr is not None:
+            dt = datetime.datetime.strptime(int_gr.group(), '%Y%m%d').date()
+            video_dates.append(dt)
+        else:
+            video_dates.append('date_not_available')
+        len_lst = np.append(lenght, spllited_lst)
+        spllited_dir.append(len_lst)
+        output_df = pd.DataFrame(spllited_dir, columns=['length', 'dir_name', 'dir_name1', 'dir_name2', 'dir_name3', 'dir_name4',
+                                                        'dir_name5', 'dir_name6'])
+        output_df.insert(0, 'info_from_brackets', bracket_info_lst)
+        output_df.insert(1, 'video_date', video_dates)
+    return output_df
+
+output_df2 = split_dir_path_for_2021(df2, dir_path_name_str = 'directory_path',
+                   parent_folder_path = 'Z:\\Clinical_Database\\surgery_ot_data\\',
+                   substring = '\\', date_digit_pattern = '\d{8}')
+
+final_df2 = pd.concat([df2, output_df2], axis=1)
+
+final_df2.to_excel('D:\\Shweta\\surgery_images_ss\\output_df\\2021_06_30_sub_dirs(2021).xlsx', index=False)
+
+##
+string = 'Y:\RESEARCH\RESEARCH-TEAM\Smita Sister\All surgical images\2014-June2019\Aarti BBay (RC)'
+string = 'Y:\RESEARCH\RESEARCH-TEAM\Smita Sister\All surgical images\2014-June2019\Anita Baheti( RC-skin sparing)'
+
+result = re.search(r"\(([-A-Za-z0-9_ ]+)\)", string)
+print(result.group(1))
+
+# 'U+2702' or 'U+0022'
+
+##
+# string_pattern = '\d{8}'
+# string1 = 'Z:\\Clinical_Database\\surgery_ot_data\\2021_05\\CHANCHALABAI OSTWAL\\20210514100306_903063\\video_files'
+# gr = re.search(string_pattern, string1)
+# dt = datetime.datetime.strptime(gr.group(), '%Y%m%d').date()
+# dt
